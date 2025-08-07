2fc88be03070fb345aa2734358bf37e542a5d1ef
karmaicom <karmaicom@gmail.com>
2025-08-06 23:58:10 -0300

    feat: Controller, Requests e Responses para User.

---
+++ src/Backend/MyRecipeBook.Api/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpPost("/register")]
+        [ProducesResponseType(type: typeof(UserRegisterResponse), statusCode: StatusCodes.Status201Created)]
+        public IActionResult Register([FromBody] UserRegisterRequest request)
+        {
+            return Created();
+        }
+
+    }
+}
